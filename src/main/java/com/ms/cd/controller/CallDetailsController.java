67ce72e2fa17f6525b026998bd668c89c557de07
SWARUP <SWARUP@SWARUP>
2021-05-03 23:42:43 +0530

    #

--- src/main/java/com/ms/cd/controller/CallDetailsController.java
+++ src/main/java/com/ms/cd/controller/CallDetailsController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class CallDetailsController {
 	@Autowired
 	private CallDetailsService callDetailsService;
 	
-	
+	@GetMapping(value = "/customer/{phonrNo}/calldetails", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<CallDetailsDto> getCustomerCallDetails(@PathVariable long phoneNo){
 		
 		logger.info("CallDetails request for customer ",phoneNo);
